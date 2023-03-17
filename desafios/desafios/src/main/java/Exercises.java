f71ce0136dab7535ab8f020aa1547aae142e28ca
gabriel-fc <gfc@ic.ufal.br>
2023-03-17 14:11:59 -0300

    implements two method: fibonnaci and question2. Fibocci uses an array to save all numbers in the sequence and question 2 checks if the number passed is present in the sequence. also adds test for question2

--- desafios/desafios/src/main/java/Exercises.java
+++ desafios/desafios/src/main/java/Exercises.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Exercises {
 
     public static int question1(int index){
@@ -9,5 +11,28 @@ public class Exercises {
         }
         System.out.println(sum);
         return sum;
+    }
+
+    public static int fibonacci(int number, int[] sequence){
+        if(number < 2){
+            sequence[number] = number;
+            return number;
+        }
+        sequence[number-1] = sequence[number-1] != -1 ? sequence[number-1] : fibonacci(number-1, sequence);
+        sequence[number-2] = sequence[number-2] != -1 ? sequence[number-2] : fibonacci(number-2, sequence);
+        sequence[number] = sequence[number-1] + sequence[number-2];
+        return sequence[number];
+    }
+
+    public static String question2(int number){
+        String output = "O número %d %s aparece na sequência!";
+        int[] sequence = new int[number+1];
+        Arrays.fill(sequence, -1);
+        fibonacci(number, sequence);
+        for (int value:
+             sequence) {
+            if(value == number) return String.format(output, number, "");
+        }
+        return String.format(output, number, "não");
     }
 }
--- desafios/desafios/src/test/java/ExercisesTest.java
+++ desafios/desafios/src/test/java/ExercisesTest.java
@@ -1,10 +1,23 @@
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import static org.junit.Assert.assertEquals;
 
 public class ExercisesTest {
     @Test
     public void testQuestion1(){
         assertEquals(55, Exercises.question1(10));
+    }
+
+
+    @Test
+    public void testQuestion2(){
+        String expectedMessage = "O número %d %s aparece na sequência!";
+
+        assertEquals(String.format(expectedMessage, 0, ""), Exercises.question2(0));
+        assertEquals(String.format(expectedMessage, 1, ""), Exercises.question2(1));
+        assertEquals(String.format(expectedMessage, 2, "não"), Exercises.question2(2));
+        assertEquals(String.format(expectedMessage, 9, "não"), Exercises.question2(9));
     }
 }
